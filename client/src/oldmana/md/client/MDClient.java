c7d4b53818a45ae58a029fe8763cf3605a83ced3
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2021-01-09 19:18:17 -0800

    Update Version

--- client/src/oldmana/md/client/MDClient.java
+++ client/src/oldmana/md/client/MDClient.java
@@ -35,7 +35,7 @@ public class MDClient
 {
 	private static MDClient instance;
 	
-	public static final String VERSION = "0.6 PR2";
+	public static final String VERSION = "0.6 PR3";
 	
 	private MDFrame window;
 	
