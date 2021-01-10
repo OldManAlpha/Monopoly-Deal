734e42ffdbf2e3497c65bd2cc98a0806d2d1349c
Brett <theoldmanalpha@gmail.com>
2021-01-09 19:18:17 -0800

    Update Version

--- server/src/oldmana/md/server/MDServer.java
+++ server/src/oldmana/md/server/MDServer.java
@@ -46,7 +46,7 @@ public class MDServer
 {
 	private static MDServer instance;
 	
-	public static final String VERSION = "0.6 PR2";
+	public static final String VERSION = "0.6 PR3";
 	
 	private List<MDMod> mods = new ArrayList<MDMod>();
 	
