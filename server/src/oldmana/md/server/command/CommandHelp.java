49aab42bbbb68ebc85103f4c5b01c47e27bac09b
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2021-07-28 19:02:08 -0700

    Help command can now provide usage on a specific command

--- server/src/oldmana/md/server/command/CommandHelp.java
+++ server/src/oldmana/md/server/command/CommandHelp.java
@@ -1,21 +1,38 @@
 package oldmana.md.server.command;
 
+import oldmana.md.server.ChatColor;
 import oldmana.md.server.CommandSender;
 
 public class CommandHelp extends Command
 {
 	public CommandHelp()
 	{
-		super("help", null, new String[] {"/help"}, true);
+		super("help", null, new String[] {"/help <Command Name>"}, true);
 	}
 	
 	@Override
 	public void executeCommand(CommandSender sender, String[] args)
 	{
-		sender.sendMessage("List of all commands:");
-		for (Command cmd : getServer().getCommandHandler().getCommands())
+		if (args.length > 0)
 		{
-			cmd.sendUsage(sender);
+			Command cmd = getServer().getCommandHandler().findCommand(args[0]);
+			if (cmd != null)
+			{
+				sender.sendMessage("Usage of " + cmd.getName() + ":");
+				cmd.sendUsage(sender);
+			}
+			else
+			{
+				sender.sendMessage(ChatColor.PREFIX_ALERT + "Command not found.");
+			}
+		}
+		else
+		{
+			sender.sendMessage("List of all commands:");
+			for (Command cmd : getServer().getCommandHandler().getCommands())
+			{
+				cmd.sendUsage(sender);
+			}
 		}
 	}
 }
