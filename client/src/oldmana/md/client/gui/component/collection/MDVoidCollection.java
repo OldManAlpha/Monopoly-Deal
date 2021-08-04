8a9dc14dd5b2bda852d9f6b824ab97b582be5c06
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2021-08-03 22:25:44 -0700

    Cards going into the void get eaten after arriving

--- client/src/oldmana/md/client/gui/component/collection/MDVoidCollection.java
+++ client/src/oldmana/md/client/gui/component/collection/MDVoidCollection.java
@@ -8,6 +8,7 @@ import java.awt.RenderingHints;
 
 import oldmana.md.client.MDEventQueue.CardMove;
 import oldmana.md.client.MDScheduler.MDTask;
+import oldmana.md.client.card.Card;
 import oldmana.md.client.card.collection.CardCollection;
 import oldmana.md.client.gui.util.GraphicsUtils;
 
@@ -16,6 +17,8 @@ public class MDVoidCollection extends MDCardCollectionUnknown
 	private int stage; // Range: 0(invisible) to 30(fully visible)
 	private double pos; // Range: -20 to 20
 	private boolean dir;
+	
+	private int intoVoidTicks;
 	
 	public MDVoidCollection(CardCollection collection)
 	{
@@ -38,7 +41,7 @@ public class MDVoidCollection extends MDCardCollectionUnknown
 					dir = !dir;
 				}
 				
-				if (isCardIncoming() || isCardMovingFrom() || getClient().isDebugEnabled())
+				if (isCardIncoming() || isCardMovingFrom() || getClient().isDebugEnabled() || intoVoidTicks > 0)
 				{
 					stage = Math.min(30, stage + 1);
 					repaint();
@@ -48,8 +51,17 @@ public class MDVoidCollection extends MDCardCollectionUnknown
 					stage--;
 					repaint();
 				}
+				
+				intoVoidTicks = Math.max(intoVoidTicks - 1, 0);
 			}
 		});
+	}
+	
+	@Override
+	public void cardArrived()
+	{
+		super.cardArrived();
+		intoVoidTicks = 30;
 	}
 	
 	private boolean isCardMovingFrom()
@@ -78,6 +90,7 @@ public class MDVoidCollection extends MDCardCollectionUnknown
 	public void paintComponent(Graphics gr)
 	{
 		Graphics2D g = (Graphics2D) gr;
+		Graphics2D gv = (Graphics2D) g.create();
 		if (getCollection() == null)
 		{
 			return;
@@ -89,8 +102,19 @@ public class MDVoidCollection extends MDCardCollectionUnknown
 			g.setColor(new Color(0, 0, 40));
 			double size = (double) stage / 30;
 			g.fillRoundRect((getWidth() / 2) - (int) (GraphicsUtils.getCardWidth() * size * 0.5), (getHeight() / 2) - 
-					(int) (GraphicsUtils.getCardHeight() * size * 0.5), 	(int) (GraphicsUtils.getCardWidth() * size), 
+					(int) (GraphicsUtils.getCardHeight() * size * 0.5), (int) (GraphicsUtils.getCardWidth() * size), 
 					(int) (GraphicsUtils.getCardHeight() * size), scale(10 * size), scale(10 * size));
+			
+			if (intoVoidTicks > 0)
+			{
+				double intoVoidProg = intoVoidTicks / 30.0;
+				gv.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				gv.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+				gv.drawImage(Card.getBackGraphics(1), (getWidth() / 2) - (int) (GraphicsUtils.getCardWidth() * size * 0.5 * intoVoidProg), (getHeight() / 2) - 
+						(int) (GraphicsUtils.getCardHeight() * size * 0.5 * intoVoidProg), (int) (GraphicsUtils.getCardWidth() * size * intoVoidProg), 
+						(int) (GraphicsUtils.getCardHeight() * size * intoVoidProg), null);
+			}
+			
 			if (getClient().isDebugEnabled())
 			{
 				g.setColor(Color.ORANGE);
