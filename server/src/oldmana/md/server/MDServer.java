afbe9a3c6597ee17bbe23e47449f64e895b1b673
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2021-08-27 17:50:06 -0700

    Bump version

--- server/src/oldmana/md/server/MDServer.java
+++ server/src/oldmana/md/server/MDServer.java
@@ -57,7 +57,7 @@ public class MDServer
 {
 	private static MDServer instance;
 	
-	public static final String VERSION = "0.6.3";
+	public static final String VERSION = "0.6.3.1";
 	
 	private List<MDMod> mods = new ArrayList<MDMod>();
 	
