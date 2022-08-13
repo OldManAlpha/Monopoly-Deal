fce76b855997fd7ef019e078fc92b83f38388472
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2022-08-12 18:42:44 -0700

    Change version

--- client/src/main/java/oldmana/md/client/MDClient.java
+++ client/src/main/java/oldmana/md/client/MDClient.java
@@ -41,7 +41,7 @@ public class MDClient
 {
 	private static MDClient instance;
 	
-	public static final String VERSION = "0.7 Dev";
+	public static final String VERSION = "0.6.4 Dev";
 	
 	private MDFrame window;
 	
--- server/src/main/java/oldmana/md/server/MDServer.java
+++ server/src/main/java/oldmana/md/server/MDServer.java
@@ -64,7 +64,7 @@ public class MDServer
 {
 	private static MDServer instance;
 	
-	public static final String VERSION = "0.7 Dev";
+	public static final String VERSION = "0.6.4 Dev";
 	
 	private List<MDMod> mods = new ArrayList<MDMod>();
 	
