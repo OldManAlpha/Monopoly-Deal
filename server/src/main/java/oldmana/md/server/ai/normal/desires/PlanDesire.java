22ee6683a60e2cfae7249e2d5f7b814af7e55c66
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2023-06-12 22:18:15 -0700

    Update pom.xml

--- server/src/main/java/oldmana/md/server/ai/normal/desires/PlanDesire.java
+++ server/src/main/java/oldmana/md/server/ai/normal/desires/PlanDesire.java
@@ -7,11 +7,11 @@ public interface PlanDesire<T extends Plan>
 {
 	/**
 	 * Desire range: 0 to 100
-	 * 0 -> <25: Extremely low desire, will not execute unless a move must be made
-	 * 25 -> <50: Enough desire to execute the plan if there's nothing better to do
-	 * 50 -> <75: High desire to execute the plan
-	 * 75 -> 100: Plan is extremely lucrative and takes high priority in execution
-	 * <0: Plan is illegal to perform
+	 * 0 - 25: Extremely low desire, will not execute unless a move must be made
+	 * 25 - 50: Enough desire to execute the plan if there's nothing better to do
+	 * 50 - 75: High desire to execute the plan
+	 * 75 - 100: Plan is extremely lucrative and takes high priority in execution
+	 * 0: Plan is illegal to perform
 	 */
 	double getDesire(Player player);
 }
