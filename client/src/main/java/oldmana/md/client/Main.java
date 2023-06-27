34a011aa16b631b03b04fdf33522e3789051377c
OldManAlpha <60587722+OldManAlpha@users.noreply.github.com>
2023-06-27 14:36:22 -0700

    Set properties to increase client performance

--- client/src/main/java/oldmana/md/client/Main.java
+++ client/src/main/java/oldmana/md/client/Main.java
@@ -2,11 +2,20 @@ package oldmana.md.client;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		List<String> argsList = Arrays.asList(args);
+		if (!argsList.contains("noj2dprops"))
+		{
+			System.setProperty("sun.java2d.transaccel", "true");
+			System.setProperty("sun.java2d.d3d", "false");
+			System.setProperty("sun.java2d.ddforcevram", "true");
+		}
 		SwingUtilities.invokeLater(() ->
 		{
 			try
